1382122efa91639bab79b8780ce7187d7db66edf
Xer0X <57611392+dr-dba@users.noreply.github.com>
2021-02-13 20:03:12 +0200

    Add files via upload

---
+++ _macroinit.lua
@@ -0,0 +1,28 @@
+﻿require("Lib-Common-@Xer0X")
+require("introspection-@Xer0X")
+local	ok, err_msg, le = Xer0X.fnc_safe_require("LE")
+if	ok 
+then	_G.LE = le
+	_G.le = le
+end
+if not	debug.traceback__orig 
+and	debug.getinfo(debug.traceback, "S").what == "C" 
+then	debug.traceback__orig = debug.traceback
+end
+local ok, err_msg, stp = Xer0X.fnc_safe_require("StackTracePlusPlus-@Xer0X")
+Xer0X.STP = stp
+local sz_err_dir = win.GetEnv("temp")
+if stp and true
+then	debug.traceback = function(...)
+		local err_rep_1, err_rep_2, err_rep_3, err_rep_4, err_rep_5 = debug.traceback__orig(...)
+		Xer0X.fnc_file_text_save(sz_err_dir.."\\far_err_rpt_orig.txt", err_rep_1)
+		local tbl_stack, tbl_path = stp.fnc_stack_trace(..., 2, "no_header", true)
+		Xer0X.fnc_file_text_save(sz_err_dir.."\\far_err_rpt_plus.txt", tbl_stack.message_new)
+		far.Timer(1, function(sender)
+			sender.Enabled = false;
+			sender:Close();
+			LE(tbl_stack, "EXEC ", nil, nil, tbl_path)
+		end)
+		return err_rep_1.."\n"..tbl_stack.message_new
+	end
+end
