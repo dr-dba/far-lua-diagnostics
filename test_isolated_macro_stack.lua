aecfda91e2eb4c1b94bd3f93a5a4a1fd2351b013
Xer0X <57611392+dr-dba@users.noreply.github.com>
2021-02-24 21:18:43 +0200

    Add files via upload

---
+++ test_isolated_macro_stack.lua
@@ -0,0 +1,26 @@
+﻿-- [[
+if true then return end --]] 
+
+--[[
+Пример исползования
+Что используем:
+1.) Загрузку (можно и выгрузку вышеуказанным способом) одичного фаяла макросов
+ИЗ ПРОИЗВОЛьНОЙ ДИРЕКТОРИИ без перезагрузки
+(обратите внимание из какой трешевой папки это было загружено)
+2.) Использование StackTracePlus@Х для просмотра контекста 
+(не как показыватель ошибок)
+3.) Можно наставить таких tracebacks в любом месте кода, 
+и различать их по переданного типа маркера,
+в примере это "Hello World"
+-- ]]
+
+require("StackTracePlusPlus-@Xer0X")
+
+Macro { description = "my cool macro test";
+	area = "Editor"; key = "CtrlSpace";
+	priority = 100;
+	action = function()
+		local abc = 12345
+		Xer0X.STP.traceback("Hello World!")
+	end;
+}
